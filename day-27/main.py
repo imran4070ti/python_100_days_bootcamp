dada63a75df8b9ba2116911cc5ac029d36a934ee
imran4070ti <imranhasan4070ti@gmail.com>
2024-06-14 03:02:34 +0600

    start day-27

---
+++ day-27/main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('My first GUI Program')
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tk.Label(text='I am a Label', font=('Arial', 24, 'bold'))  # To create a label on the window.
+my_label.pack(side='left')  # To show the created label on the window.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()  # Like a while loop that basically keeps the window open.
