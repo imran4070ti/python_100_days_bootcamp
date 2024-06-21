00f7c2fb55b7b6e70437d45815f0258db885b282
imran4070ti <imranhasan4070ti@gmail.com>
2024-06-21 21:52:41 +0600

    day 27

--- day-27/main.py
+++ day-27/main.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 window.title('My first GUI Program')
 window.minsize(width=500, height=300)
 
-# Label
+# Labellabel
 my_label = tk.Label(text='I am a Label', font=('Arial', 24, 'bold'))  # To create a label on the window.
 my_label.pack(side='left')  # To show the created label on the window.
 
